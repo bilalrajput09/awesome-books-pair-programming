8b7c5235b5da9cd15874e76dc4ab70b1b1f66813
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-03-20 14:54:16 +0100

    Fix linters

--- js/main.js
+++ js/main.js
@@ -1,68 +1,57 @@
 // Select elements
 
-const bookList = document.getElementsByClassName("book_list")[0];
-const addBtn = document.getElementsByClassName("btn_form")[0];
-
+const bookList = document.getElementsByClassName('book_list')[0];
+const addBtn = document.getElementsByClassName('btn_form')[0];
+const removeBtn = document.getElementsByClassName('remove');
 let books = [];
 
-addBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const title = document.getElementById('title').value.trim();
-    const author = document.getElementById('author').value.trim();
-    addBooks(title, author);
-    display();
-});
-
-
-
-
-const addBooks = (title, author) => {
-    if (title != "" && author != "") {
-        obj = {
-            Title: title,
-            Author: author,
-        }
-        books.push(obj);
-        localStorage.setItem('Books', JSON.stringify(books));
-    }
-}
-
 function display() {
-    let display = ``;
-    books.forEach((sec, i) => {
-        console.log(sec, i);
-        display += `
+  let display = '';
+  books.forEach((sec, i) => {
+    display += `
       <div>
         <p>${sec.Title}</p>
         <p>${sec.Author}</p>
-        <button onclick = "remove(${i})">Remove</button>
+        <button class = "remove" onclick = "remove(${i})">Remove</button>
       </div>
     `;
-
-    });
-    bookList.innerHTML = display;
-
+  });
+  bookList.innerHTML = display;
 }
-
-const remove = (id) => {
-    const BookIndex = books.findIndex((item, i) => {
-        id === i;
-    });
-    books.splice(BookIndex, 1);
+const addBooks = (title, author) => {
+  if (title !== '' && author !== '') {
+    const obj = {
+      Title: title,
+      Author: author,
+    };
+    books.push(obj);
     localStorage.setItem('Books', JSON.stringify(books));
-    display();
+  }
+};
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value.trim();
+  const author = document.getElementById('author').value.trim();
+  addBooks(title, author);
+  display();
+});
+
+function remove(id) {
+  const BookIndex = books.findIndex((item, i) => id === i);
+  books.splice(BookIndex, 1);
+  localStorage.setItem('Books', JSON.stringify(books));
+  display();
 }
+
+removeBtn.addEventListener('click', remove);
 
 function checkLocalStorage() {
-    if (localStorage.getItem("Books") == null) {
-        books = [];
-    }
-    else {
-        books = JSON.parse(localStorage.getItem("Books"));
-        display();
-    }
-
+  if (localStorage.getItem('Books') == null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('Books'));
+    display();
+  }
 }
 
 checkLocalStorage();
-
