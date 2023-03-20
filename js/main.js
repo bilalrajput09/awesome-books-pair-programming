ea8956aca9549cb2e4287ea6e96e5fca579b5b2b
JOdiemo <odiemo@gmail.com>
2023-03-20 13:36:37 +0300

    Implement add and display function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 // Select elements
 
 const bookList = document.getElementsByClassName("book_list")[0];
+const addBtn = document.getElementsByClassName("btn_form")[0];
 
+let books = [];
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value.trim();
+  const author = document.getElementById('author').value.trim();
+  addBooks(title, author);
+  display();
+});
+
+
+
+
+const addBooks = (title, author) => {
+  if (title != "" && author != ""){
+    obj = {
+      Title: title,
+      Author: author,
+    }
+    books.push(obj);
+  }
+}
+
+function display(){
+  let display = ``;
+  books.forEach((sec, i) => {
+    display += `
+      <div>
+        <p>${sec.Title}</p>
+        <p>${sec.Author}</p>
+        <button>Remove</button>
+      </div>
+    `;
+
+  });
+  bookList.innerHTML = display;
+}
 
